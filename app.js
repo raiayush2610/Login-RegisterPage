620630ba651e0cdc680e225d7e9b70743891f9ee
Ayush Rai <ayushayushravi@gmail.com>
2022-05-02 10:35:44 +0530

    LoginRegisterPage

---
+++ app.js
@@ -0,0 +1,114 @@
+require('dotenv').config();
+const express= require("express");
+const bodyParser=require("body-parser");
+const ejs = require("ejs");
+const mongoose =require("mongoose");
+// const encrypt =require("mongoose-encryption");
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+// const md5 =require("md5")
+
+const app= express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+  }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+mongoose.connect("mongodb://localhost:27017/hositalDatabase");
+
+const userSchema= new mongoose.Schema({
+    email: String,
+    password:String,
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+ const User=new mongoose.model("User",userSchema);
+
+ passport.use(User.createStrategy());
+
+ passport.serializeUser(User.serializeUser());
+ passport.deserializeUser(User.deserializeUser());
+
+
+app.get("/",function(req,res){
+    res.render("main");
+});
+app.get("/login",function(req,res){
+    res.render("login");
+});
+
+app.get("/register",function(req,res){
+    
+    res.render("register");
+});
+
+
+app.get("/logout",function(req,res){
+    req.logout();
+    res.redirect("/");
+});
+app.get("/succes",function(req,res){
+    if (req.isAuthenticated()){
+        res.render("succes")
+    }else{
+        res.redirect("/login");
+    }
+});
+
+app.post("/register",function(req,res){
+    
+
+    User.register({username: req.body.username}, req.body.password, function(err,user){
+        if (err) {
+            try{
+                res.render("errors",{error:err});
+            }
+            catch(err){
+                res.send(err.message);
+
+            }
+            
+           
+        }else{
+            passport.authenticate("local")(req,res, function(){
+                res.redirect("succes");
+            });
+        }
+
+    });
+    
+ });
+  
+app.post("/login",function(req,res){
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password 
+    });
+    req.login(user,function(err){
+        if (err){
+            console.log(err);           
+        }else{
+            passport.authenticate("local")(req,res, function(){
+                res.redirect("succes");
+            });
+
+        }
+    });
+ });
+
+ 
+ app.listen(process.env.PORT|| 3300,function(){
+     console.log("Server is running on port 3300");
+ });
